3c69c458b89ab76539c407f264854b1950aa17a2
JuliaShubia <juliassshubina@gmail.com>
2026-08-31 10:00:45 +0300

    initial commit
    Старт проекта

---
+++ navigator/navigator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  navigator
+//
+//  Created by Юля Шубина on 31.08.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
